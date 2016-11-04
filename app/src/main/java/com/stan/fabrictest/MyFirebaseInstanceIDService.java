a185790531777b9d5967047f6b6297abf1eb902d
stanho <stanhe@4dxm.com>
2016-11-04 18:15:45 +0800

    no message

---
+++ app/src/main/java/com/stan/fabrictest/MyFirebaseInstanceIDService.java
@@ -0,0 +1,27 @@
+package com.stan.fabrictest;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by stan on 16/11/4.
+ */
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    public static final String TAG = "MyFirebaseInstanceIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+
+    }
+}
